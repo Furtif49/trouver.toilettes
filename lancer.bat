a3f8805a6dcafb7a0774b018ec9e453dbdcbb45e
shibabalin <lescoconis@gmail.com>
2026-06-03 23:05:51 +0200

    ajout .gitignore, retrait lancer.bat et .claude

--- lancer.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Lancement du serveur local...
-echo Ouvre http://localhost:8000 dans ton navigateur
-echo (appuie sur Ctrl+C pour arreter)
-cd /d "%~dp0"
-python -m http.server 8000
-pause
